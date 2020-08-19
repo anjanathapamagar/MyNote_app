45e7a790e793a2c2f8f32ab188f52f9a3cb2585e
Jibit Khanal <khanaljibit80@gmail.com>
2020-08-19 20:41:54 +0545

    Initial commit

---
+++ app/src/main/java/com/example/mynoteapp/MainActivity.java
@@ -0,0 +1,25 @@
+package com.example.mynoteapp;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.widget.Button;
+import android.widget.ImageView;
+import android.widget.TextView;
+
+public class MainActivity extends AppCompatActivity {
+    ImageView imageView, imageview5;
+    TextView txt1;
+    Button btm1, btm2;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        imageView = findViewById(R.id.imageView);
+        btm1 = findViewById(R.id.btm1);
+        btm2 = findViewById(R.id.btm2);
+        imageview5 = findViewById(R.id.imageView5);
+        txt1 = findViewById(R.id.txt1);
+    }
+}
